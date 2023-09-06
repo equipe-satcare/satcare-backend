2cd64088a352795b8b669834ef14b0767f926fdc
darabas <gmd.darabas@hotmail.com>
2023-09-05 23:45:40 -0300

    Adiciona Horario de Atendimento

---
+++ src/main/java/com/satc/satcdisciplinabackend/model/CommonEntity.java
@@ -0,0 +1,5 @@
+package com.satc.satcdisciplinabackend.model;
+
+public abstract class CommonEntity {
+    private Integer id;
+}
---
+++ src/main/java/com/satc/satcdisciplinabackend/model/HorarioAtendimento.java
@@ -0,0 +1,11 @@
+package com.satc.satcdisciplinabackend.model;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+public class HorarioAtendimento {
+    private Funcionario funcionario;
+    private DayOfWeek diaSemana;
+    private LocalTime horaInicio;
+    private LocalTime horaFim;
+}
