45395433cb1b118a78f7591fc58639f7c3b71c7e
Rubens Scotti Junior <128634484+rubensscotti@users.noreply.github.com>
2023-09-06 14:41:59 -0300

    Create Servico.java
    Criação da classe serviço. Nessa classe será informado qual é o serviço, o tipo, o valor unitário e o tempo mínimo de execução.

---
+++ src/main/java/com/satc/satcdisciplinabackend/model/Servico.java
@@ -0,0 +1,8 @@
+package com.satc.satcdisciplinabackend.model;
+
+public class Servico {
+	private String nome;
+	private TipoServico tipoServico;
+	private BigDecimal valor;
+	private Integer tempo;
+}
