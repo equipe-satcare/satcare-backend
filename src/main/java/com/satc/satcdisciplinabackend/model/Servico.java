31c19e82dde9733b69d9c057e22a0ad3b4ec7e9d
darabas <gmd.darabas@hotmail.com>
2023-09-06 14:42:45 -0300

    Adiciona Servico

---
+++ src/main/java/com/satc/satcdisciplinabackend/model/Servico.java
@@ -0,0 +1,10 @@
+package com.satc.satcdisciplinabackend.model;
+
+import java.math.BigDecimal;
+
+public class Servico {
+    private String nome;
+    private TipoServico tipoServico;
+    private BigDecimal valor;
+    private Integer tempo;
+}
