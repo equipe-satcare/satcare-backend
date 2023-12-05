3cb669c55dcadc4ae8dcec31828c5637cf44b02a
Juliano <jcfjulianojcf@gmail.com>
2023-12-04 23:04:43 -0300

    Regra de negocio #4

--- src/main/java/com/satc/satcdisciplinabackend/service/AgendamentoService.java
+++ src/main/java/com/satc/satcdisciplinabackend/service/AgendamentoService.java
@@ -1,18 +1,24 @@
 package com.satc.satcdisciplinabackend.service;
 
 import com.satc.satcdisciplinabackend.model.Agendamento;
+import com.satc.satcdisciplinabackend.model.Funcionario;
 import com.satc.satcdisciplinabackend.model.Servico;
 import com.satc.satcdisciplinabackend.repository.AgendamentoRepository;
+import com.satc.satcdisciplinabackend.repository.FuncionarioRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class AgendamentoService extends CommonServiceImpl<Agendamento> {
     @Autowired
     AgendamentoRepository repository;
+
+    @Autowired
+    FuncionarioRepository funcionarioRepository;
 
     @Autowired
     public AgendamentoService(ModelMapper modelMapper, AgendamentoRepository repository) {
@@ -39,6 +45,22 @@ public class AgendamentoService extends CommonServiceImpl<Agendamento> {
         boolean conflitoCliente = repository.possuiConflitoCliente(dataHoraInicio, dataHoraFinal, agendamento.getCliente());
         if (conflitoCliente) {
             System.out.println("Conflito de horario do funcionario");
+            // todo mensagem de erro
+            return null;
+        }
+
+        Funcionario funcionario = funcionarioRepository.findById(agendamento.getFuncionario().getId()).get();
+        List<Integer> servicosIdsFuncionario = funcionario.getServicos()
+                .stream()
+                .map(Servico::getId)
+                .toList();
+
+        // verifica se algum servicoId do agendamento nao esta na lista de servicos do funcionario
+        boolean possuiServicoInvalido = agendamento.getServicos().stream()
+                .anyMatch(servico -> !servicosIdsFuncionario.contains(servico.getId()));
+
+        if (possuiServicoInvalido) {
+            System.out.println("Lista de servicos invalida");
             // todo mensagem de erro
             return null;
         }
