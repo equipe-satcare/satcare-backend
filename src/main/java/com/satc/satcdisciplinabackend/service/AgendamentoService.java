c3b26b62b8f477c4daa7b43f8a7a9d0af1a4561e
Darabas <gmd.darabas@hotmail.com>
2023-12-04 23:14:56 -0300

    calcular valor total agendamento

--- src/main/java/com/satc/satcdisciplinabackend/service/AgendamentoService.java
+++ src/main/java/com/satc/satcdisciplinabackend/service/AgendamentoService.java
@@ -1,18 +1,30 @@
 package com.satc.satcdisciplinabackend.service;
 
 import com.satc.satcdisciplinabackend.model.Agendamento;
+import com.satc.satcdisciplinabackend.model.Funcionario;
 import com.satc.satcdisciplinabackend.model.Servico;
 import com.satc.satcdisciplinabackend.repository.AgendamentoRepository;
+import com.satc.satcdisciplinabackend.repository.FuncionarioRepository;
+import com.satc.satcdisciplinabackend.repository.ServicoRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AgendamentoService extends CommonServiceImpl<Agendamento> {
     @Autowired
     AgendamentoRepository repository;
+
+    @Autowired
+    FuncionarioRepository funcionarioRepository;
+
+    @Autowired
+    ServicoRepository servicoRepository;
 
     @Autowired
     public AgendamentoService(ModelMapper modelMapper, AgendamentoRepository repository) {
@@ -21,13 +33,19 @@ public class AgendamentoService extends CommonServiceImpl<Agendamento> {
 
     @Override
     public Agendamento save(Agendamento agendamento) {
-        int tempoAtendimento = agendamento.getServicos().stream()
+        List<Servico> servicos = servicoRepository.findAllById(agendamento.getServicos().stream().map(Servico::getId).collect(Collectors.toList()));
+        int tempoAtendimento = servicos.stream()
                 .mapToInt(Servico::getTempo)
                 .sum();
+
+        BigDecimal valorTotal = servicos.stream()
+                .map(Servico::getValor)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
 
         LocalDateTime dataHoraInicio = agendamento.getDataHoraInicio();
         LocalDateTime dataHoraFinal = dataHoraInicio.plusMinutes(tempoAtendimento);
         agendamento.setDataHoraFim(dataHoraFinal);
+        agendamento.setValorTotal(valorTotal);
 
         boolean conflitoFuncionario = repository.possuiConflitoFuncionario(dataHoraInicio, dataHoraFinal, agendamento.getFuncionario());
         if (conflitoFuncionario) {
@@ -42,6 +60,8 @@ public class AgendamentoService extends CommonServiceImpl<Agendamento> {
             // todo mensagem de erro
             return null;
         }
+
+
 
         return super.save(agendamento);
     }
