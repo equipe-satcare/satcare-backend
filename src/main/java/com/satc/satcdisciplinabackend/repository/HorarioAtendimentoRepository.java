1e935a774b74296990f4760e8919497477121cec
StephanAnthony <stephanusa07@hotmail.com>
2023-12-04 21:58:20 -0300

    Feito Horario de Atendimento

---
+++ src/main/java/com/satc/satcdisciplinabackend/repository/HorarioAtendimentoRepository.java
@@ -0,0 +1,8 @@
+package com.satc.satcdisciplinabackend.repository;
+
+import com.satc.satcdisciplinabackend.model.HorarioAtendimento;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HorarioAtendimentoRepository extends CommonRepository<HorarioAtendimento> {
+}
